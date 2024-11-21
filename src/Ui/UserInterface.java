42323fc54b306b9fedc4751580226ca0b5839046
gustav-hr <gure0001@stud.kea.dk>
2024-11-21 16:07:49 +0100

    Ui update

--- src/Ui/UserInterface.java
+++ src/Ui/UserInterface.java
@@ -11,19 +11,27 @@ public class UserInterface {
         System.out.println("Please choose what profile you want to enter:");
         System.out.println("Dolphin leader" + "\nDolphin treasurer" + "\nDolphin coach");
         String userInput = "";
-        userInput = scanner.next().toLowerCase();
+
         while (!userInput.equalsIgnoreCase("exit")) {
+            userInput = scanner.next().toLowerCase();
+            switch (userInput) {
 
-            while (userInput.equalsIgnoreCase("leader")) {
-                System.out.println("You have signed in as leader of the Dolphin swimming club. ");
-                System.out.println("Your options as the leader of the Dolphin are:");
-                System.out.println("Type: 'add' to add members to the swimming club. ");
-                System.out.println("Type 'remove' to remove members from the swimming club. ");
-                System.out.println("Type 'update' to update information on the members in the swimming club. ");
+                case "leader" -> {
+                    System.out.println("You have signed in as leader of the Dolphin swimming club. ");
+                    System.out.println("Your options as the leader of the Dolphin are:");
+                    System.out.println("Type: 'add' to add members to the swimming club. ");
+                    System.out.println("Type 'remove' to remove members from the swimming club. ");
+                    System.out.println("Type 'update' to update information on the members in the swimming club. ");
+                    System.out.println("Type 'exit' to exit the program. ");
 
-                //  -----------------------------------------------------------------------------------------------------------
-            } // CODE FOR THE LEADER OF THE SWIMMING CLUB.
+
+                }
+                case "exit" -> {
+                    System.out.println("You are now exiting the program...");
+                    System.exit(0);
+
+                }
+            }
         }
     }
 }
-
