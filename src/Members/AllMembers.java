52bd61ae47b034cbcfa802255816a29d53593a23
gustav-hr <gure0001@stud.kea.dk>
2024-11-21 15:50:47 +0100

    TXT + getters&setters

---
+++ src/Members/AllMembers.java
@@ -0,0 +1,39 @@
+package Members;
+
+public class AllMembers {
+
+    private String name;
+    private String status;
+    private int age;
+
+    public void Member (String name, String status, int age) {
+        this.name=name;
+        this.status=status;
+        this.age=age;
+    }
+    // GETTER & SETTER FOR NAME ------------------------------------------------------------------------------------
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    // GETTER & SETTER FOR AGE ------------------------------------------------------------------------------------
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    // GETTER & SETTER FOR STATUS ------------------------------------------------------------------------------------
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
--- src/Ui/UserInterface.java
+++ src/Ui/UserInterface.java
@@ -1,17 +1,29 @@
 package Ui;
 
+import java.util.Scanner;
+
 public class UserInterface {
+    private final Scanner scanner = new Scanner(System.in); // Final scanner to use in the UI.
 
-    public static void main(String[] args) {
 
+    public void startProgram() {
         System.out.println("Welcome to the Dolphin swimming club!");
         System.out.println("Please choose what profile you want to enter:");
-        System.out.println("Dolphin leader" + "\n Dolphin treasurer)" + "\n Dolphin coach");
+        System.out.println("Dolphin leader" + "\nDolphin treasurer" + "\nDolphin coach");
+        String userInput = "";
+        userInput = scanner.next().toLowerCase();
+        while (!userInput.equalsIgnoreCase("exit")) {
 
-        String uesrInput = "";
+            while (userInput.equalsIgnoreCase("leader")) {
+                System.out.println("You have signed in as leader of the Dolphin swimming club. ");
+                System.out.println("Your options as the leader of the Dolphin are:");
+                System.out.println("Type: 'add' to add members to the swimming club. ");
+                System.out.println("Type 'remove' to remove members from the swimming club. ");
+                System.out.println("Type 'update' to update information on the members in the swimming club. ");
 
-        while (uesrInput.equalsIgnoreCase("leader")) {
-            System.out.println("You have signed in as leader of the Dolhpin swimming club. ");
+                //  -----------------------------------------------------------------------------------------------------------
+            } // CODE FOR THE LEADER OF THE SWIMMING CLUB.
         }
     }
 }
+
