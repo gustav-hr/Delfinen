813d016bff8a1835a326b8318b10fd7577841edd
gustav-hr <gure0001@stud.kea.dk>
2024-11-21 14:36:31 +0100

    Fundament

--- src/Ui/UserInterface.java
+++ src/Ui/UserInterface.java
@@ -2,5 +2,16 @@ package Ui;
 
 public class UserInterface {
 
+    public static void main(String[] args) {
 
+        System.out.println("Welcome to the Dolphin swimming club!");
+        System.out.println("Please choose what profile you want to enter:");
+        System.out.println("Dolphin leader" + "\n Dolphin treasurer)" + "\n Dolphin coach");
+
+        String uesrInput = "";
+
+        while (uesrInput.equalsIgnoreCase("leader")) {
+            System.out.println("You have signed in as leader of the Dolhpin swimming club. ");
+        }
+    }
 }
