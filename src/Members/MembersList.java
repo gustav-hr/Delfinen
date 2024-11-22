7996c70a4e85eb3ba2a7993ecd05a9832e90c613
BertramHansen <beha0007@stud.kea.dk>
2024-11-22 11:55:17 +0100

    addmembers function done

--- src/Members/MembersList.java
+++ src/Members/MembersList.java
@@ -7,8 +7,13 @@ public class MembersList {
     private ArrayList<AllMembers> members = new ArrayList<>();
     // This is the ArrayList where we gather the added members.
 
-    public void addMember(String name, String status, int age) {
-        AllMembers member = new AllMembers(name, status, age);
+    public void addWorkoutSwimmer(String name, String status, int age) {
+        AllMembers member = new WorkoutSwimmer(name, status, age);
+        members.add(member);
+    }
+
+    public void addCompetitionSwimmer(String name, String status, int age, String coach, double breastTime, double crawlTime, double backCrawlTime, double butterflyTime){
+        AllMembers member = new CompetitionSwimmer(name,status,age,coach,breastTime,crawlTime,backCrawlTime,butterflyTime);
         members.add(member);
     }
 
