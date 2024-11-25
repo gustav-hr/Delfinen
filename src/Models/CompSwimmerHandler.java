c4b1588f15504652c754e8767267d5ea359133b7
BertramHansen <beha0007@stud.kea.dk>
2024-11-25 11:54:49 +0100

    CompSwimmerHandler done 2

--- src/Models/CompSwimmerHandler.java
+++ src/Models/CompSwimmerHandler.java
@@ -62,35 +62,3 @@ public class CompSwimmerHandler {
         return memberList;
     }
 }
-/*
-while (scan.hasNextLine()) {
-// Remove label parts and extract the actual values
-// The use of "replace" can be seen in the .txt. This means that we won't see the "title" before the name of the movie.
-// Easiest to see under the txt file how it works.
-//Replace method found on: https://w3schools.com/java/ref_string_replace.asp
-String title = scan.nextLine().replace("Title: ", "").trim();
-String director = scan.nextLine().replace("Director: ", "").trim();
-int yearCreated = Integer.parseInt(scan.nextLine().replace("Year created: ", "").trim());
-String isInColor = scan.nextLine().replace("Is in color? :", "").trim();
-int lengthInMinutes = Integer.parseInt(scan.nextLine().replace("Length in minutes: ", "").trim());
-String genre = scan.nextLine().replace("Genre: ", "").trim();
-
-Movie movie = new Movie(title, director, yearCreated, isInColor, lengthInMinutes, genre);
-                movieList.add(movie);
-
-                if (scan.hasNextLine()) {
-        scan.nextLine(); // Skip separator line
-                }
-                        }
-                        System.out.println("Movie collection loaded successfully.");
-        } catch (FileNotFoundException e) {
-        throw new RuntimeException("File not found: " + e.getMessage());
-        } catch (InputMismatchException | NumberFormatException e) {
-        throw new RuntimeException("Error reading file, format mismatch: " + e.getMessage());
-        }
-        return movieList;
-    }
-            } *
-
-
- */
